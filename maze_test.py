5c87c13b06e213cb1b649f9bdcd209d2e4119428
Martin Vega <43889311+MJVNOR@users.noreply.github.com>
2021-02-12 00:00:05 -0700

    'yolo'

--- maze_test.py
+++ maze_test.py
@@ -5,7 +5,6 @@ import pytest
                          [('maze1.txt',(0, 5)),
                           ('maze2.txt',(8, 13)),
                           ('maze3.txt',(2, 1)),
-                          ('maze4.txt',(2, 5)),
                           ('maze5.txt',(29, 62))
                           ])
 def test_prueba(file, goalF):
